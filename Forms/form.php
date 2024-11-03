ec2a4e0167f7e4826cf3590051ca3c9f4da0ad59
Astra 1999 <mdmuntasirfahad@gmail.com>
2024-11-03 21:04:57 +0600

    form

---
+++ Forms/form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Work+Sans:ital,wght@0,600;1,600&display=swap"
+        rel="stylesheet">
+</head>
+<body>
+<nav>
+        <div class="logo">
+        </div>
+
+        <div class="nav-con">
+            <ul class="nav-ul">
+                <li><a class="nav-a" href="#">Home</a></li>
+                <li><a class="nav-a" href="#">Contact</a></li>
+                <li><a class="nav-a" href="#">About Us</a></li>
+            </ul>
+        </div>
+
+        <div class="nav-btn">
+            <a href="#"><button class="primary_btn">Login</button></a>
+            <a href="#"><button class="primary_btn">Sign Up</button></a>
+        </div>
+    </nav>
+</body>
+</html>
